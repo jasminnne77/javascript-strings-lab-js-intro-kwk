54729f4e38375438146850b5bfe9f3438c478a41
jasminne77 <jasminea50331@students.laalliance.org>
2016-11-02 15:31:51 +0000

    First saved version of my first nitrous

---
+++ index.js
@@ -0,0 +1,19 @@
+var greeting = "Hello, everybody!";
+
+var specialGuest = "Neil deGrasse Tyson"
+
+var greetSpecialGuest = "greetSpecialGuest" + specialGuest + "!";
+
+var topic = "space";
+
+var conversation = `Let\'s talk about ${topic}.`;
+
+var writeGreetingToDocument = `something to do with document...`;
+
+// Type your answer into the console without the quote marks and see if it works.
+
+var customCelebration = 'Kaboomie! Hachewie, yeaaaaaaaah. Shwing.';
+
+var consoleCelebration = (`console.log(customCelebration);`)';
+
+
